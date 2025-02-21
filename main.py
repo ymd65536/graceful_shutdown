4687fbeb09b8d586035402abdf229ac65bc03153
Kento.Yamada <ymd65536@outlook.com>
2025-02-21 13:37:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import threading
 import time
 import datetime
 
-WAIT_SECONDS_AFTER_SIGTERM = 5  # SIGTERMを受け取ってから強制終了するまでの待機時間
+WAIT_SECONDS_AFTER_SIGTERM = 1  # SIGTERMを受け取ってから強制終了するまでの待機時間
 
 sigterm_event = Event()
 exit_event = Event()
@@ -32,11 +32,14 @@ def handle_sigterm(signum, frame):
     if not exit_event.wait(WAIT_SECONDS_AFTER_SIGTERM):
         print(f"{WAIT_SECONDS_AFTER_SIGTERM} seconds have passed. Force exit...")
 
+        count = 0
         # ここに強制終了前に必要な後処理を書く
         while count_threads("service_thread") > 0:
-            print("wait exit child threads.")
+            print(f"wait exit child threads.{count}")
             time.sleep(1)
-
+            count += 1
+        time_now = datetime.datetime.now()
+        print(time_now)
         can_gracefully_exit = True
         exit_event.set()
         sys.exit(0)
@@ -53,13 +56,12 @@ def handle_sigterm(signum, frame):
 
 
 def service():
-    while not sigterm_event.is_set():
-        # 一回に時間がかかる処理
-        print("loop start")
-        time_now = datetime.datetime.now()
-        print(time_now)
-        time.sleep(6)
-        print("loop finish")
+    # 一回に時間がかかる処理
+    print("loop start")
+    time_now = datetime.datetime.now()
+    print(time_now)
+    time.sleep(20)
+    print("loop finish")
     exit_event.set()
 
 
@@ -71,6 +73,6 @@ if __name__ == "__main__":
         target=service,
         name="service_thread"
     )
-    service_thread.daemon = True  # メインスレッドの終了時にservice_threadも終了する
+    # service_thread.daemon = True  # メインスレッドの終了時にservice_threadも終了する
     service_thread.start()
     exit_event.wait()
